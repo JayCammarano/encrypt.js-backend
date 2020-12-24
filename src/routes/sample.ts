9741d2ed8479059ae854ff375ef9a9e980f1a9cf
jaycammarano <jaycammarano@gmail.com>
2020-12-24 18:08:41 -0500

    removed extra file

--- src/routes/sample.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from 'express';
-
-const serverHealthCheck = (res: Response) => {
-  return res.status(200).json({
-    message: 'pong'
-  });
-};
-
-export default { serverHealthCheck };
