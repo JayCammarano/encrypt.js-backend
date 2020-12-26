f339aac8cd0c6c912c92f652d8ca50b4a023e58a
jaycammarano <jaycammarano@gmail.com>
2020-12-25 21:29:22 -0500

    validates info before sending it to the db

--- src/middleware/validInfo.ts
+++ src/middleware/validInfo.ts
@@ -2,21 +2,28 @@ import { Request, Response, NextFunction } from 'express';
 export const validInfo = (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
 
-  function validUsername(username: string) {
-    return /^\w+([\.-]?\w+)\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(username);
-  }
+  const validUsername = (username: string) => {
+    return /^(?=[a-zA-Z0-9._]{3,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/.test(username);
+  };
+  const validPassword = (password: string) => {
+    return /^(?=[a-zA-Z0-9._]{3,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/.test(password);
+  };
 
   if (req.path === '/register') {
     if (![username, password].every(Boolean)) {
       return res.status(401).json('Missing Credentials');
     } else if (!validUsername(username)) {
       return res.status(401).json('Invalid username');
+    } else if (!validPassword) {
+      return res.status(401).json('Invalid password');
     }
   } else if (req.path === '/signin') {
     if (![username, password].every(Boolean)) {
       return res.status(401).json('Missing Credentials');
     } else if (!validUsername(username)) {
       return res.status(401).json('Invalid username');
+    } else if (!validPassword) {
+      return res.status(401).json('Invalid password');
     }
   }
 
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,7 @@
 import { Router, Response } from 'express';
 import { signUp, signIn } from '../controllers/auth';
-import { validInfo } from '../middleware/validInfo';
 import { authorizer } from '../middleware/authorization';
+import { validInfo } from '../middleware/validInfo';
 const router = Router();
 
 router.post('/register', validInfo, signUp);
