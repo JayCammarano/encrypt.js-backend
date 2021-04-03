3fdfb3dcea7d1f966ab05d3b4cc5bcce5dd20d6a
jaycammarano <jaycammarano@gmail.com>
2021-04-03 13:19:44 -0400

    set up a dev docker Co-authored-by: Rob <rmcfadzean@users.noreply.github.com>

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,8 @@
 import { Pool } from 'pg';
 
 export const pool: Pool = new Pool({
-  user: process.env.DB_USERNAME || 'postgres',
-  host: process.env.DBHOST ||'localhost',
+  user: process.env.POSTGRES_USER|| 'postgres',
+  host: process.env.POSTGRES_HOST ||'localhost',
   port: 5432,
-  database: process.env.DBNAME || 'encrypted_events_ts_' + process.env.NODE_ENV
+  database: process.env.POSTGRES_DB || 'encrypted_events_ts_' + process.env.NODE_ENV
 });
