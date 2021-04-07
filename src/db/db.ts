91b6ae457cc035da19cbdfbba9921318c48ec8cc
jaycammarano <jaycammarano@gmail.com>
2021-04-07 16:04:50 -0400

    added ssl

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,5 @@
 import { Pool } from 'pg';
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL+"?ssl=true"
 
 export const pool = new Pool({
   connectionString,
