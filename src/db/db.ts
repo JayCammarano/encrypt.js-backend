1282e5cebd3c82fc255db1c1ab563ec99ad58d15
jaycammarano <jaycammarano@gmail.com>
2021-04-07 15:43:46 -0400

    uses DATABASE_URL and connectionString for pool

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,6 @@
 import { Pool } from 'pg';
+const connectionString = process.env.DATABASE_URL
 
-export const pool: Pool = new Pool({
-  user: process.env.POSTGRES_USER|| 'postgres',
-  host: process.env.POSTGRES_HOST ||'localhost',
-  port: 5432,
-  database: process.env.POSTGRES_DB || 'encrypted_events_ts_' + process.env.NODE_ENV
-});
+export const pool = new Pool({
+  connectionString,
+})
